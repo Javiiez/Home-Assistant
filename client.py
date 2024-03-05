50be908427998d85843d13d86f85d9bd9c07e233
Javier Alemán <108713280+Javiiez@users.noreply.github.com>
2024-03-05 14:24:37 -0500

    Update client.py

--- client.py
+++ client.py
@@ -12,40 +12,28 @@ mqtt_password = "Javier05"
 mqtt_transport = "tcp"
 mqtt_keepalive = 60
 mqtt_client_id = "core-mosquitto"
-
-# MQTT Topic
 mqtt_topic = "home/front_door/distance"
 
 def on_connect(client, userdata, flags, rc, properties=None):
     if rc == 0:
         print("INFO: Connected to MQTT Broker")
+        # Subscribe to the topic
+        client.subscribe(mqtt_topic)
     else:
         print("ERROR: Connection failed with code", rc)
 
-def build_payload():
-    payload_hash = {
-        "unique_id": "ultrasonic_sensor_1",
-        "name": "Ultrasonic Sensor",
-        "state_topic": mqtt_topic,
-        "device_class": "distance",
-        "unit_of_measure": "cm",
-        "distance": 10,
-        "qos": 0,
-        "retain": True
-    }
-    return json.dumps(payload_hash)
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload.decode("utf-8"))
+    print("Received message:", payload)
+    # Process the received message here
 
 if __name__ == "__main__":
     # Connect to MQTT Broker
-    client = mqtt.Client(mqtt.Client, client_id=mqtt_client_id, protocol=mqtt.MQTTv5, transport=mqtt_transport)
+    client = mqtt.Client(client_id=mqtt_client_id, protocol=mqtt.MQTTv5, transport=mqtt_transport)
     client.username_pw_set(username=mqtt_username, password=mqtt_password)
+    client.on_connect = on_connect
+    client.on_message = on_message
     client.connect(host=mqtt_server_host, port=mqtt_server_port, keepalive=mqtt_keepalive)
 
-    client.on_connect = on_connect
-    
-    # Publish Message
-    client.publish(mqtt_topic, build_payload())
-
-    # Disconnect from MQTT Broker
-    client.disconnect()
-    sys.exit()
+    # Start the MQTT client loop
+    client.loop_forever()
